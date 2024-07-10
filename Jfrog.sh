0d60d7239215f30fa0faa61b571e238466d5b0f0
venu0435 <160848833+venu0435@users.noreply.github.com>
2024-07-10 17:04:34 +0530

    Update Jfrog.sh

--- Jfrog.sh
+++ Jfrog.sh
@@ -4,3 +4,20 @@ docker pull docker.bintray.io/jfrog/artifactory-oss:latest
 sudo mkdir -p /jfrog/artifactory
 sudo chown -R 1030 /jfrog/
 docker run --name artifactory -d -p 8081:8081 -p 8082:8082 -v /jfrog/artifactory:/var/opt/jfrog/artifactory docker.bintray.io/jfrog/artifactory-oss:latest
+##Run Artifactory as a service
+sudo vi /etc/systemd/system/artifactory.service
+##Copy the below code
+[Unit]
+Description=Setup Systemd script for Artifactory Container
+After=network.target
+[Service]
+Restart=always
+ExecStartPre=-/usr/bin/docker kill artifactory
+ExecStartPre=-/usr/bin/docker rm artifactory
+ExecStart=/usr/bin/docker run --name artifactory -p 8081:8081 -p 8082:8082 \
+  -v /jfrog/artifactory:/var/opt/jfrog/artifactory \
+  docker.bintray.io/jfrog/artifactory-oss:latest
+ExecStop=-/usr/bin/docker kill artifactory
+ExecStop=-/usr/bin/docker rm artifactory
+[Install]
+WantedBy=multi-user.target
